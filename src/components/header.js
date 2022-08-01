2da6556f6e868c8175fd0104d404d3bc04cccc09
Abdullah Saqib <abdullahsaqib112@users.noreply.github.com>
2022-08-01 02:25:34 +0200

    1st try

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+function Header() {
+    return (
+      <div>
+        <h1>
+            Expense Tracker App by Abdullah Saqib
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
